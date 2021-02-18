f0bb9349cbc331fbaecbda8bffdea22be8b696e7
liubin <liubin.bestme@bytedance.com>
2021-02-18 21:35:09 +0800

    feat: 完成5.4章节

--- chapter5/5.3存储分配和废料收集.scm
+++ chapter5/5.3存储分配和废料收集.scm
@@ -1,3 +1,81 @@
 
 
+;; 伪代码
+(
+ begin-garbage-collection
+ ;; 开始GC之前准备
+ (assign free (const 0))
+ (assign scan (const 0))
+ (assign old (reg root))
+ (assign relocate-continue (label reassign-root))
+ (goto (label relocate-old-result-in-new))
+ ;; 完成GC之后
+ reassign-root
+ (assign root (reg new))
+ (goto (label gc-loop))
+ gc-loop
+ ;; 检查 free 和 scan 指针是否相同
+ (test (op =) (reg free) (reg scan))
+ (branch (label gc-flip))
+ ;; 寻找到 scan 指向的car进行垃圾回收，后续更新该 car 对应位置的值
+ (assign old (op vector-ref) (reg new-cars) (reg scan))
+ (assign relocate-continue (label update-car))
+ (goto (label relocate-old-result-in-new))
+ update-car
+ ;; relocate-old-result-in-new 会将 new 赋值为新值
+ (perform (op vector-set!) (reg new-cars) (reg scan) (reg new))
+ ;; 进行 scan 指针的 cdr 垃圾回收
+ (assign old (op vector-ref) (reg new-cdrs) (reg scan))
+ (assign relocate-continue (label update-cdr))
+ (goto (label relocate-old-result-in-new))
+ update-cdr
+ (perform (op vector-set!) (reg new-cdrs) (reg scan) (reg new))
+ (assign scan (op +) (reg scan) (const 1))
+ ;; 开始下轮的垃圾回收
+ (goto (label gc-loop))
+ ;; 子程序按如下方式进行重新分配：
+ ;; 若要求重新分配的对象（由old指向）不是序对，子程序就返回指向该对象的同一个指针，并不做任何修改
+ ;; 若是序对则执行重新分配操作，若序对包含一个破碎之心标记，则说明序对已被移走，因此提出其中的前向地址（cdr位置），并在new中返回该地址
+ ;; 若old指向尚未移动的序对，则把序对移动到新存储区的第一个自由位置（free指向），将破碎之心标记和前向地址存入序对的老地址，设置好破碎之心。
+ ;; relocate-old-result-in-new 用寄存器oldcr保存由old指向的对象的car或cdr
+ ;; old ，new，前向地址是指向存储数组的下标
+ relocate-old-result-in-new
+ (test (op pointer-to-pair?) (reg old))
+ (branch (label pair))
+ (assign new (reg old))
+ (goto (reg relocate-continue))
+ pair
+ (assign oldcr (op vector-ref) (reg the-cars) (reg old))
+ (test (op broken-heart?) (reg oldcr))
+ (branch (label already-moved))
+ (assign new (reg free))
+ ;; 递增free
+ (assign free (op +) (reg free) (const 1))
+ ;; 更新 new-cars
+ (perform (op vector-set!)
+          (reg new-cars) (reg new) (reg oldcr))
+ (assign oldcr (op vector-ref) (reg the-cdrs) (reg old))
+ ;; 更新 new-cdrs
+ (perform (op vector-set!)
+          (reg new-cdrs) (reg new) (reg oldcr))
+ ;; 将 old 更新为 broken-heart
+ (perform (op vector-set!)
+          (reg the-cars) (reg old) (const broken-heart))
+ (perform (op vector-set!)
+          (reg the-cdrs) (reg old) (reg new))
+ (goto (reg relocate-continue))
+ already-moved
+ (assign new (op vector-ref) (reg the-cdrs) (reg old))
+ (goto (reg relocate-continue))
+ ;; 废料收集最后，交换老存储区和新存储区的对象，只需交换指针的值：the-cars与new-cars呼唤，the-cdrs和new-cdrs互换
+ gc-flip
+ (assign temp (reg the-cars))
+ (assign the-cars (reg new-cars))
+ (assign new-cars (reg temp))
+ (assign temp (reg the-cdrs))
+ (assign the-cdrs (reg new-cdrs))
+ (assign new-cdrs (reg temp))
+ )
+
+
 
---
+++ chapter5/5.4显示控制的求值器.scm
@@ -0,0 +1,264 @@
+(load "5.2-regsim-self.scm")
+(load "5.4-scm-machine-lib.scm")
+
+;; 需要的operatoins
+(define eceval-operatons
+  (list
+   ;;primitive Scheme operations
+   (list 'read read)
+
+   ;;operations in syntax.scm
+   (list 'self-evaluating? self-evaluating?)
+   (list 'quoted? quoted?)
+   (list 'text-of-quotation text-of-quotation)
+   (list 'variable? variable?)
+   (list 'assignment? assignment?)
+   (list 'assignment-variable assignment-variable)
+   (list 'assignment-value assignment-value)
+   (list 'definition? definition?)
+   (list 'definition-variable definition-variable)
+   (list 'definition-value definition-value)
+   (list 'lambda? lambda?)
+   (list 'lambda-parameters lambda-parameters)
+   (list 'lambda-body lambda-body)
+   (list 'if? if?)
+   (list 'if-predicate if-predicate)
+   (list 'if-consequent if-consequent)
+   (list 'if-alternative if-alternative)
+   (list 'begin? begin?)
+   (list 'begin-actions begin-actions)
+   (list 'last-exp? last-exp?)
+   (list 'first-exp first-exp)
+   (list 'rest-exps rest-exps)
+   (list 'application? application?)
+   (list 'operator operator)
+   (list 'operands operands)
+   (list 'no-operands? no-operands?)
+   (list 'first-operand first-operand)
+   (list 'rest-operands rest-operands)
+
+   ;;operations in eceval-support.scm
+   (list 'true? true?)
+   (list 'make-procedure make-procedure)
+   (list 'compound-procedure? compound-procedure?)
+   (list 'procedure-parameters procedure-parameters)
+   (list 'procedure-body procedure-body)
+   (list 'procedure-environment procedure-environment)
+   (list 'extend-environment extend-environment)
+   (list 'lookup-variable-value lookup-variable-value)
+   (list 'set-variable-value! set-variable-value!)
+   (list 'define-variable! define-variable!)
+   (list 'primitive-procedure? primitive-procedure?)
+   (list 'apply-primitive-procedure apply-primitive-procedure)
+   (list 'prompt-for-input prompt-for-input)
+   (list 'announce-output announce-output)
+   (list 'user-print user-print)
+   (list 'empty-arglist empty-arglist)
+   (list 'adjoin-arg adjoin-arg)
+   (list 'last-operand? last-operand?)
+   (list 'no-more-exps? no-more-exps?)	;for non-tail-recursive machine
+   (list 'get-global-environment get-global-environment)))
+
+(define the-global-environment (setup-environment))
+
+(define scm-machine
+  (make-machine
+   '(exp env val continue proc argl unev)
+   eceval-operatons
+   '(read-eval-print-loop
+     (perform (op initialize-stack))
+     (perform (op prompt-for-input) (const ";;;EC-eval input::"))
+     (assign exp (op read))
+     (assign env (op get-global-environment))
+     (assign continue (label print-result))
+     (goto (label eval-dispatch))
+     print-result
+     (perform (op announce-output) (const ";;;EC-eval output::"))
+     (perform (op user-print) (reg val))
+     (goto (label read-eval-print-loop))
+     eval-dispatch
+     (test (op self-evaluating?) (reg exp))
+     (branch (label ev-self-eval))
+     (test (op variable?) (reg exp))
+     (branch (label ev-variable))
+     (test (op quoted?) (reg exp))
+     (branch (label ev-quoted))
+     (test (op assignment?) (reg exp))
+     (branch (label ev-assignment))
+     (test (op definition?) (reg exp))
+     (branch (label ev-definition))
+     (test (op if?) (reg exp))
+     (branch (label ev-if))
+     (test (op lambda?) (reg exp))
+     (branch (label ev-lambda))
+     (test (op begin?) (reg exp))
+     (branch (label ev-begin))
+     (test (op application?) (reg exp))
+     (branch (label ev-application))
+     (goto (label unknown-expression-type))
+     ;; 简单表达式求值
+     ev-self-eval
+     (assign val (reg exp))
+     (goto (reg continue))
+     ev-variable
+     (assign val (op lookup-variable-value) (reg exp) (reg env))
+     (goto (reg continue))
+     ev-quoted
+     (assign val (op text-of-quotation) (reg exp))
+     (goto (reg continue))
+     ev-lambda
+     (assign unev (op lambda-parameters) (reg exp))
+     (assign exp (op lambda-body) (reg exp))
+     (assign val (op make-procedure)
+             (reg unev) (reg exp) (reg env))
+     (goto (reg continue))
+     ;; 过程应用求值
+     ev-application
+     (save continue)
+     (save env)
+     (assign unev (op operands) (reg exp))
+     (save unev)
+     ;; 先求值运算符
+     (assign exp (op operator) (reg exp))
+     (assign continue (label ev-appl-did-operator))
+     (goto (label eval-dispatch))
+     ;; 求值运算对象初始化
+     ev-appl-did-operator
+     (restore unev)
+     (restore env)
+     (assign argl (op empty-arglist))
+     ;; 将 proc 设置为求值运算符产生出的过程
+     (assign proc (reg val))
+     (test (op no-operands?) (reg unev))
+     ;; 没参数则直接执行
+     (branch (label apply-dispatch))
+     (save proc)
+     ;; 取出一个参数进行计算求值
+     ev-appl-operand-loop
+     (save argl)
+     (assign exp (op first-operand) (reg unev))
+     (test (op last-operand?) (reg unev))
+     (branch (label ev-appl-last-arg))
+     (save env)
+     (save unev)
+     (assign continue (label ev-appl-accumulate-arg))
+     (goto (label eval-dispatch))
+     ;; 求值完一个参数后，积累到 argl 寄存器中，并从 unev 中删除
+     ev-appl-accumulate-arg
+     (restore unev)
+     (restore env)
+     (restore argl)
+     (assign argl (op adjoin-arg) (reg val) (reg argl))
+     (assign unev (op rest-operands) (reg unev))
+     (goto (label ev-appl-operand-loop))
+     ev-appl-last-arg
+     (assign continue (label ev-appl-accum-last-arg))
+     (goto (label eval-dispatch))
+     ev-appl-accum-last-arg
+     (restore argl)
+     (assign argl (op adjoin-arg) (reg val) (reg argl))
+     (restore proc)
+     ;; 将参数应用于过程
+     (goto (label apply-dispatch))
+     ;; 过程应用
+     apply-dispatch
+     (test (op primitive-procedure?) (reg proc))
+     (goto (label primitive-apply))
+     (test (op compound-procedure?) (reg proc))
+     (goto (label compound-apply) (reg proc))
+     (goto (label unknown-procedure-type))
+     primitive-apply
+     (assign val (op apply-primitive-procedure) (reg proc) (reg argl))
+     (restore continue)
+     (goto (reg continue))
+     compound-apply
+     (assign unev (op procedure-parameters) (reg proc))
+     (assign env (op procedure-environment) (reg proc))
+     (assign env (op extend-environment) (reg unev) (reg argl) (reg env))
+     (assign unev (op procedure-body) (reg proc))
+     (goto (label ev-sequence))
+     ev-begin
+     (assign unev (op begin-actions) (reg proc))
+     (save continue)
+     (goto (label ev-sequence))
+     ev-sequence
+     (assign exp (op first-exp) (reg unev))
+     (test (op last-exp?) (reg unev))
+     (branch (label ev-sequence-last-exp))
+     (save unev)
+     (save env)
+     (assign continue (label ev-sequence-continue))
+     (goto (label eval-dispatch))
+     ev-sequence-continue
+     (restore env)
+     (restore unev)
+     (assign unev (op rest-exps) (reg unev))
+     (goto (label ev-sequence))
+     ev-sequence-last-exp
+     (restore continue)
+     (goto (label eval-dispatch))
+     ev-if
+     (save exp)
+     (save env)
+     (save continue)
+     (assign continue (label ev-if-decide))
+     (assign exp (op if-predicate) (reg exp))
+     (goto (label eval-dispatch))
+     ev-if-decide
+     (restore continue)
+     (restore env)
+     (restore exp)
+     (test (op true?) (reg val))
+     (branch (label ev-if-consequent))
+     ev-if-alternative
+     (assign exp (op if-alternative) (reg exp))
+     (goto (label eval-dispatch))
+     ev-if-consequent
+     (assign exp (op if-consequent) (reg exp))
+     (goto (label eval-dispatch))
+     ;; 赋值和定义
+     ev-assignment
+     (assign unev (op assignment-variable) (reg exp))
+     (save unev)
+     (assign exp (op assignment-value) (reg exp))
+     (save env)
+     (save continue)
+     (assign continue (label ev-assignment-l))
+     (goto (label eval-dispatch))
+     ev-assignment-l
+     (restore continue)
+     (restore env)
+     (restore unev)
+     (perform (op set-variable-value!) (reg unev) (reg val) (reg env))
+     (assign val (const ok))
+     (goto (reg continue))
+     ev-definition
+     (assign unev (op definition-variable) (reg exp))
+     (save unev)
+     (assign exp (op definition-value) (reg exp))
+     (save env)
+     (save continue)
+     (assign continue (label ev-definition-l))
+     (goto (label eval-dispatch))
+     ev-definition-l
+     (restore continue)
+     (restore env)
+     (restore unev)
+     (perform (op define-variable!) (reg unev) (reg val) (reg env))
+     (assign val (const ok))
+     (goto (reg continue))
+     unknown-expression-type
+     (assign val (const unknown-expression-type-error))
+     (goto (label signal-error))
+     unknown-procedure-type
+     (assign val (const unknown-procedure-type-error))
+     (goto (label signal-error))
+     signal-error
+     (perform (op user-print) (reg val))
+     (goto (label read-eval-print-loop))
+     )))
+
+
+(start scm-machine)
+
+
